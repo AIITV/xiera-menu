b7b155c06377c2d9e3b2d0c11ee03d871ba7301c
AIITV <113987436+AIITV@users.noreply.github.com>
2022-11-10 11:08:05 +0100

    +uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,23 @@
+#!usr/bin/env bash
+
+name=$(whoami)
+
+whiptail --title "CONFIRMATION" --yesno "Do you want to start byob?" 8 78 
+if [[ $? -eq 0 ]]; then 
+  sleep 1
+elif [[ $? -eq 1 ]]; then 
+  whiptail --title "MESSAGE" --msgbox "Cancelling Process" 8 78 
+elif [[ $? -eq 255 ]]; then 
+  whiptail --title "MESSAGE" --msgbox "User pressed ESC. Exiting the script" 8 78 
+fi
+
+sudo apt-get remove whiptail -y
+sudo apt-get remove 
+
+cd /home/"$name"/
+rm -r xiera-menu
+
+
+
+
+
